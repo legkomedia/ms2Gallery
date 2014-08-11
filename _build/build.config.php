833aae45b7e9135dc32bdc4e7b314c3b517f72d8
Bezumkin <bezumkin@yandex.ru>
2014-08-11 10:27:21 +0700

    Fixed static plugins

--- _build/build.config.php
+++ _build/build.config.php
@@ -43,7 +43,7 @@ define('BUILD_PERMISSION_UPDATE', true);
 
 define('BUILD_CHUNK_STATIC', false);
 define('BUILD_SNIPPET_STATIC', false);
-define('BUILD_PLUGIN_STATIC', true);
+define('BUILD_PLUGIN_STATIC', false);
 
 $BUILD_RESOLVERS = array(
 	'tables',
